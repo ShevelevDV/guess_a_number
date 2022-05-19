c113b34d3be757c704cf4aa898530f6999efd67c
ShevelevDV <Shevelev.dmitri@gmail.com>
2022-05-19 20:53:31 +0300

    v. 1.0

--- my_game.py
+++ my_game.py
@@ -1,27 +1,30 @@
 import numpy as np
 
-low=1
-mid=50
-high=101
-count=0
-search_range=[low,mid,high]
-number = np.random.randint(1, 101) # загадываем число
-
-while True:
-    
-    count += 1
-    predict_number = np.random.randint(low, high)
-    
-    if predict_number == number:
-        break
-    elif search_range[0]<predict_number<search_range[1]:
-        high=search_range[1]
-        mid=round((low+high)/2)
-    elif search_range[1]<predict_number<search_range[2]:
-        low=search_range[1]
-        mid=round((low+high)/2)
-    
+def gues_number():
+    low=1
+    mid=50
+    high=101
+    count=0
+    search_range=[low,mid,high]
     print(search_range)
+    number = np.random.randint(1, 101) # загадываем число
     
-    
-print(count)
+
+    while True:
+        
+        predict_number = np.random.randint(search_range[0], search_range[2])
+        mid=predict_number
+        count += 1
+
+        if predict_number == number:
+            break
+        elif search_range[0]<number<search_range[1]:
+            high=search_range[1]
+            mid=round((low+high)/2)
+        elif search_range[1]<number<search_range[2]:
+            low=search_range[1]
+            mid=round((low+high)/2)
+
+        search_range=[low,mid,high]
+     
+    return(count)
